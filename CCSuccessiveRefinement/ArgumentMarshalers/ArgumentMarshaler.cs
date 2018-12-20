752370e5b6fc2ddcb6e71c0ec12653b7c182886a
BuildTools <unconfigured@null.spigotmc.org>
2018-12-20 17:17:40 -0600

    Initial Checkin

---
+++ CCSuccessiveRefinement/ArgumentMarshalers/ArgumentMarshaler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCSuccessiveRefinement
+{
+    public interface ArgumentMarshaler
+    {
+        void Set(IEnumerator<string> currentArgument);
+    }
+}
